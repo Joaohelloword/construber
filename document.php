433dd6cb267c8f41517884982fee9cb600f751b1
Joaohelloword <152641324+Joaohelloword@users.noreply.github.com>
2024-05-23 13:18:55 -0300

    Update document.php

--- document.php
+++ document.php
@@ -7,14 +7,13 @@ $email = addslashes($_POST['email']);
 $para = "joaoanime.com@gmail.com";
 $assunto = "cadastro constrube";
 
-$corpo = "RG: ".$rg"\n"."cpf".$cpf."\n"."cnh".$cnh"."\n"."CPF :".$email.;
+$corpo = "RG: ".$rg."\n"."cpf".$cpf."\n"."cnh".$cnh"."\n"."CPF :".$email;
 
-$cabeça = "from: joaoanime.com@gmail.com"."\n"."reply-to"$email"."\n"."X-Maile:PHP/".phpversion();
+$cabeça = "from: joaoanime.com@gmail.com"."\n"."Reply-to"$email"."\n"."X-Maile:PHP/".phpversion();
 
 if(mail($para,$assunto,$corpo,$cabeça)){
-    echo("email-enviado")
-}
-else{
+    echo("email-enviado");
+}else{
     echo("hove um erro");
 }
 
